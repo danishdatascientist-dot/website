abb096396c9b520289e3e024863f0929d029f00f
AbdulRehman2040 <146017169+AbdulRehman2040@users.noreply.github.com>
2026-07-09 14:37:56 +0500

    pushed

--- app/page.tsx
+++ app/page.tsx
@@ -14,6 +14,7 @@ import Blog from './components/Blog';
 import YouTube from './components/YouTube';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
+import Chatbot from './components/Chatbot';
 import './globals.css';
 
 export default function Home() {
@@ -34,6 +35,7 @@ export default function Home() {
           <Contact />
         </main>
         <Footer />
+        <Chatbot />
       </div>
     </ThemeProvider>
   );
